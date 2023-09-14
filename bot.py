452b3628b11dc36da343ab1653a4d5b8d0488cfd
chelokot <andrey.vlasenko.work@gmail.com>
2023-09-14 18:51:38 +0300

    change danbooru caption

--- bot.py
+++ bot.py
@@ -739,7 +739,7 @@ def query_text(inline_query):
                     id=post['id'],
                     photo_url=post['file_url'],
                     thumbnail_url=post['preview_file_url'],
-                    caption=f"{post['tag_string'][:480]}{'...' if len(post['tag_string']) > 480 else ''} \ndanbooru.donmai.us/posts/{post['id']}\n R:{post['rating']} | Score:{post['score']} | {post['file_ext']}\n\nUsed seacrh query: {tags}",
+                    caption=f"{post['tag_string'][:360]}{'...' if len(post['tag_string']) > 360 else ''}\n\nArtist: {post['tag_string_artist']} | Character: {post['tag_string_character']} \n\ndanbooru.donmai.us/posts/{post['id']}\n R:{post['rating']} | Score:{post['score']} | {post['file_ext']}\n\nUsed seacrh query: {tags}",
                 ))
             except Exception as e:
                 print(e)
